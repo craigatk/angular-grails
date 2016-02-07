c500fffbf514eb09f6c834ac2d9fd1807e7d8bb7
catkinson <catkinson@bloomhealthco.com>
2016-02-06 20:26:48 -0600

    Initial commit of Grails project generated wtih Angular profile

---
+++ grails-app/assets/javascripts/angular/grails/angular.grails.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /angular/grails/core/angular.grails.core
+//= require /angular/grails/index/angular.grails.index
+
+angular.module("angular.grails", [
+    "angular.grails.core",
+    "angular.grails.index"
+]);
---
+++ grails-app/controllers/angular/grails/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package angular.grails
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view: 'index')
+        "500"(view: '/application/serverError')
+        "404"(view: '/application/notFound')
+    }
+}
